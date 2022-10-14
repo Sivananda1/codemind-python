dbdf611e1160cc5194e9b4d1b33e0cc8d078b3fc
Sivananda1 <110597219+Sivananda1@users.noreply.github.com>
2022-10-14 15:52:21 +0530

    a=int(input()) for w in range(a):     n=int(input())     s=[]     t=[]     for i in range(n//2,n):         for j in range(2,i):             if i%j==0:                 break         else:             s.append(i)     for k in range(n,n*2):         for l in range(2,i):             if k%l==0:                 break         else:             t.append(k)     v=max(s)     u=min(t)     if n-u <= v-n:         print(v)     else:         print(u)

---
+++ Nearest_Prime.py
@@ -0,0 +1,23 @@
+a=int(input())
+for w in range(a):
+    n=int(input())
+    s=[]
+    t=[]
+    for i in range(n//2,n):
+        for j in range(2,i):
+            if i%j==0:
+                break
+        else:
+            s.append(i)
+    for k in range(n,n*2):
+        for l in range(2,i):
+            if k%l==0:
+                break
+        else:
+            t.append(k)
+    v=max(s)
+    u=min(t)
+    if n-u <= v-n:
+        print(v)
+    else:
+        print(u)
