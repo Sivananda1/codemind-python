f564c41758dad39aed8b12e9abd40075529e6e65
Sivananda1 <110597219+Sivananda1@users.noreply.github.com>
2022-10-26 20:03:02 +0530

    a,b=map(int,input().split()) l1=list(map(int,input().split())) l2=list(map(int,input().split())) l1=set(l1) l2=set(l2) s=[] t=[] for i in l1:     s.append(i) for j in l2:     s.append(j) for k in range(len(s)):     if s.count(s[k])==1:         t.append(s[k]) print(len(t))

---
+++ Count_elements_II.py
@@ -0,0 +1,15 @@
+a,b=map(int,input().split())
+l1=list(map(int,input().split()))
+l2=list(map(int,input().split()))
+l1=set(l1)
+l2=set(l2)
+s=[]
+t=[]
+for i in l1:
+    s.append(i)
+for j in l2:
+    s.append(j)
+for k in range(len(s)):
+    if s.count(s[k])==1:
+        t.append(s[k])
+print(len(t))
