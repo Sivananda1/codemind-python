a25c0b9615b0bc9f7e3355f1e65feb10833d08ee
Sivananda1 <110597219+Sivananda1@users.noreply.github.com>
2022-10-31 19:46:29 +0530

    def fun(n):     s=0     while n:         r=n%10         s+=1         n=n//10     return s n=int(input()) l=list(map(int,input().split())) t=max(l) v=fun(t) w=0 for i in l:     r=fun(i)     if r==v:         w+=1 print(w)

---
+++ Max_digits_count.py
@@ -0,0 +1,17 @@
+def fun(n):
+    s=0
+    while n:
+        r=n%10
+        s+=1
+        n=n//10
+    return s
+n=int(input())
+l=list(map(int,input().split()))
+t=max(l)
+v=fun(t)
+w=0
+for i in l:
+    r=fun(i)
+    if r==v:
+        w+=1
+print(w)
