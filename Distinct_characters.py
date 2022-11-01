c46440e7ce45fd5d6f2ce68ec45a5029ea6ea2ba
Sivananda1 <110597219+Sivananda1@users.noreply.github.com>
2022-11-01 22:45:36 +0530

    n=input().lower() n=list(n) s=[] for i in range(len(n)):     if n[i] not in s and n[i]!=' ':         s.append(n[i]) print("".join(sorted(s)))

--- Distinct_characters.py
+++ Distinct_characters.py
@@ -2,6 +2,6 @@ n=input().lower()
 n=list(n)
 s=[]
 for i in range(len(n)):
-    if n.count(n[i])==1 and n[i]!=' ':
+    if n[i] not in s and n[i]!=' ':
         s.append(n[i])
 print("".join(sorted(s)))
