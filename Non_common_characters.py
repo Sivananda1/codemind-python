6745e9c20cbc61d70478537e66540aa2686a5114
Sivananda1 <110597219+Sivananda1@users.noreply.github.com>
2022-12-07 17:20:44 +0530

    s=input() s1=input() d=[] s=s.lower() s1=s1.lower() for i in range(len(s)):     if s[i] not in s1 and s[i] not in d and s[i]!=" ":         d.append(s[i]) for i in range(len(s1)):     if s1[i] not in s and s1[i] not in d and s1[i]!=" ":         d.append(s1[i])
    print(len(d))

---
+++ Non_common_characters.py
@@ -0,0 +1,13 @@
+s=input()
+s1=input()
+d=[]
+s=s.lower()
+s1=s1.lower()
+for i in range(len(s)):
+    if s[i] not in s1 and s[i] not in d and s[i]!=" ":
+        d.append(s[i])
+for i in range(len(s1)):
+    if s1[i] not in s and s1[i] not in d and s1[i]!=" ":
+        d.append(s1[i])
+
+print(len(d))
