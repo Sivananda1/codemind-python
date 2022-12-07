e7733abfe8ab3dccd2e5eca384b6d922d47e4781
Sivananda1 <110597219+Sivananda1@users.noreply.github.com>
2022-12-07 17:15:32 +0530

    s=input() s1=input() d=[] s=s.lower() s1=s1.lower() s=s.split() s1=s1.split() for i in range(len(s1)):     if s1[i] in s:         d.append(s1[i]) if len(d)==0:     print(-1) else:     print(*d)

---
+++ common_words.py
@@ -0,0 +1,14 @@
+s=input()
+s1=input()
+d=[]
+s=s.lower()
+s1=s1.lower()
+s=s.split()
+s1=s1.split()
+for i in range(len(s1)):
+    if s1[i] in s:
+        d.append(s1[i])
+if len(d)==0:
+    print(-1)
+else:
+    print(*d)
